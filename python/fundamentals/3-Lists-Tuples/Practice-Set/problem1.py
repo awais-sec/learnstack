80e80bd5173a46ca270413dee5a447f738946ae0
Awais Ahmed <awaisavd2@gmail.com>
2026-09-03 18:44:37 +0500

    Add functionality to input and store student names
    Collects student names and stores them in a list.

---
+++ python/fundamentals/3-Lists-Tuples/Practice-Set/problem1.py
@@ -0,0 +1,14 @@
+students = []
+
+s1 = input('Enter student name: ')
+students.append(s1)
+s2 = input('Enter student name: ')
+students.append(s2)
+s3 = input('Enter student name: ')
+students.append(s3)
+s4 = input('Enter student name: ')
+students.append(s4)
+s5 = input('Enter student name: ')
+students.append(s5)
+
+print(students)
