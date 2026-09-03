a296c2d7a59675ac124ffdf3d4569e3f19b39c76
Awais Ahmed <awaisavd1@gmail.com>
2026-09-03 18:44:53 +0500

    Add files via upload

---
+++ python/fundamentals/3-Lists-Tuples/Practice-Set/problem2.py
@@ -0,0 +1,15 @@
+marks = []
+
+s1 = int(input('Enter marks name: '))
+marks.append(s1)
+s2 = int(input('Enter marks name: '))
+marks.append(s2)
+s3 = int(input('Enter marks name: '))
+marks.append(s3)
+s4 = int(input('Enter marks name: '))
+marks.append(s4)
+s5 = int(input('Enter marks name: '))
+marks.append(s5)
+
+marks.sort()
+print(marks)
---
+++ python/fundamentals/3-Lists-Tuples/Practice-Set/problem4.py
@@ -0,0 +1,3 @@
+a = (7,0,0,8,0)
+count = a.count(0)
+print(count)
